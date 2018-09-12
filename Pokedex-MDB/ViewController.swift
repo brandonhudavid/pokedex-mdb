17db1a878fe56e314dd028993a830804703171f4
Isabella Lau <izzielau@airbears2-10-142-72-106.airbears2.1918.berkeley.edu>
2018-09-11 21:36:33 -0700

    Labels for title and search.

--- Pokedex-MDB/ViewController.swift
+++ Pokedex-MDB/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var titleLabel : UILabel!
+    var searchLabel : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createTitleLabel()
+        createSearchLabel()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Load game title
+    private func createTitleLabel()
+    {
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        titleLabel.text = "Pokedex"
+        titleLabel.font = UIFont(name: "AmericanTypewriter", size: 50.0)
+        titleLabel.textAlignment = .center
+        titleLabel.center = CGPoint(x: view.frame.width / 2, y: view.frame.height - 450)
+        view.addSubview(titleLabel)
+    }
+    
+    private func createSearchLabel()
+    {
+        searchLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        searchLabel.text = "Search By"
+        searchLabel.font = UIFont(name: "AmericanTypewriter", size: 18.0)
+        searchLabel.textAlignment = .center
+        searchLabel.center = CGPoint(x: view.frame.width / 2, y: view.frame.height - 350)
+        view.addSubview(searchLabel)
+    }
 }
 
