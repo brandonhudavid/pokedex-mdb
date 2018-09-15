6c476af8558e1964b2050d11461cf74827aafd61
Brandon David <brandonhudavid@gmail.com>
2018-09-14 18:52:44 -0700

    pokemon search to profile

--- Pokedex-MDB/ProfileViewController.swift
+++ Pokedex-MDB/ProfileViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    var pokemonImage: UIImageView!
-    var pokemonName: UILabel!
-    var pokemonNumber: UILabel!
+    var pokemonName: String!
+    var pokemonNumber: String!
+    var pokemonImage: UIImage!
+    var pokemonImageView: UIImageView!
+    var pokemonNameLabel: UILabel!
+    var pokemonNumberLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pokemonImageView = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: view.frame.width / 2, height: view.frame.height / 2))
+        pokemonImageView.center = CGPoint.init(x: 100, y: 100)
+        pokemonImageView.image = pokemonImage
+        self.view.addSubview(pokemonImageView)
+        
+        pokemonNameLabel = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: view.frame.width, height: 100))
+        pokemonNameLabel.center = CGPoint.init(x: 100, y: 175)
+        pokemonNameLabel.text = pokemonName
+        pokemonNameLabel.textAlignment = .center
+        self.view.addSubview(pokemonNameLabel)
+        
+        pokemonNumberLabel = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: view.frame.width, height: 100))
+        pokemonNumberLabel.center = CGPoint.init(x: 100, y: 250)
+        pokemonNumberLabel.text = pokemonNumber
+        pokemonNumberLabel.textAlignment = .center
+        self.view.addSubview(pokemonNumberLabel)
 
         // Do any additional setup after loading the view.
     }
